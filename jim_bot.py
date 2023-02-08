a0e76df1e25ba78a89c74a0d9e6a78df792fa6b7
gaifut <gaifut@gmail.com>
2023-02-09 00:04:21 +0300

    adding a working version with venv

--- jim_bot.py
+++ jim_bot.py
@@ -2,14 +2,14 @@ import time
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
+from constants import API_KEY
 
 logging.basicConfig(level=logging.INFO)
 
 
-TOKEN = '5760224801:AAEbqZN4SUmO99F8YVTSzFItTCGnas34gXU'
 MSG1 = "Have you coded today {}?"
 
-bot = Bot(token = TOKEN)
+bot = Bot(token = API_KEY)
 dp = Dispatcher(bot = bot)
 
 @dp.message_handler(commands = ['start'])
@@ -17,12 +17,13 @@ async def start_handler(message: types.Message):
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     user_full_name = message.from_user.full_name
-    logging.INFO(f'{user_id=} {user_full_name=} {time.asctime()}')
     await message.reply(f"Hey, {user_name}!")
 
-    for i in range(10):
-        time.sleep(2)
+    for i in range(7):
+        time.sleep(60*1)
         await bot.send_message(user_id, MSG1.format(user_name))
 
+    #logging.INFO(f'{user_id=} {user_full_name=} {time.asctime()}')
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp)
